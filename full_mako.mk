223d9fed9d5cc0771d30806633491ad67ffe8184
winner00 <toverturf@gmail.com>
2012-11-27 04:06:36 -0700

    Liquify.

--- full_mako.mk
+++ full_mako.mk
@@ -17,18 +17,14 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-PRODUCT_NAME := full_mako
+PRODUCT_NAME := liquid_mako
 PRODUCT_DEVICE := mako
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Full JellyBean on Mako
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/mako/device.mk)
---
+++ liquid.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2012 The LiquidSmooth Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# name
+PRODUCT_RELEASE_NAME := N4
+
+# device
+$(call inherit-product-if-exists, device/lge/mako/full_mako.mk)
+
+# gsm
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+
+# phone
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+
+# products
+PRODUCT_DEVICE := mako
+PRODUCT_BRAND := Google
+PRODUCT_NAME := liquid_mako
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.mako.$(shell date +%m%d%y).$(shell date +%H%M%S)
+
+# overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=527662 \
+    PRODUCT_NAME=occam \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="occam-user 4.2 J0P40C 527662 release-keys" \
+    BUILD_FINGERPRINT="google/occam/mako:4.2/J0P20C/527662:user/release-keys"
+
