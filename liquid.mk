fcae359a5957bf38fa8f0f23899253ccee5cf34b
winner00 <toverturf@gmail.com>
2012-11-28 15:02:46 -0700

    Required to boot.
    Conflicts:
    	liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -27,6 +27,10 @@ $(call inherit-product, vendor/liquid/config/common_gsm.mk)
 # phone
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
 
+#Required For boot. DO NOT DELETE.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1
+
 # products
 PRODUCT_DEVICE := mako
 PRODUCT_BRAND := Google
