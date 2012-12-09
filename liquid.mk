7d96a8a65835f2306afedb0efd2bc3a2448ce767
nocoast <voteforpedro8@gmail.com>
2012-12-09 03:23:27 -0600

    Nexus4 is an international device...
    Should be treated as such ;D

--- liquid.mk
+++ liquid.mk
@@ -30,6 +30,9 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-data-only=1
 
+# locales
+PRODUCT_LOCALES := cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_AU en_CA en_GB en_NZ en_SG en_US es_ES fr_CA fr_CH fr_BE fr_FR it_CH it_IT ja_JP ka_GE ko_KR nb_NO nl_BE nl_NL nn_NO pl_PL pt_PT ru_RU sv_SE tr_TR zh_CN zh_HK zh_TW am_ET hi_I
+
 # products
 PRODUCT_DEVICE := mako
 PRODUCT_BRAND := Google
