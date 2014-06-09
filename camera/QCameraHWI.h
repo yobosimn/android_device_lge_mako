3b68891d684a65e6596ce12c374e6aa86984a0b5
PhaseBurn <phaseburn@phaseburn.net>
2014-06-09 04:21:07 -0700

    Merging tag android-4.4.3_r1.1 from AOSP into mako/Carbon
    Change-Id: Ia59e47d6abfa5fbf2951f3dc314445ed5a427097

--- camera/QCameraHWI.h
+++ camera/QCameraHWI.h
@@ -35,7 +35,6 @@
 #include <QComOMXMetadata.h>
 
 extern "C" {
-#include <linux/android_pmem.h>
 #include <linux/msm_ion.h>
 #include <mm_camera_interface2.h>
 #include "mm_omx_jpeg_encoder.h"
@@ -849,6 +848,11 @@ private:
      int                 mNoDisplayMode;
      int                 mIsoValue;
 
+     /* Used to show the process state of snapshot_jpeg_cb*/
+     Mutex                  mSnapJpegCbLock;
+     Condition              mSnapJpegCbWait;
+     bool                   mSnapJpegCbRunning;
+     bool                   mSnapCbDisabled;
 };
 
 }; // namespace android
